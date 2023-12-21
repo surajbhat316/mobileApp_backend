a397e47f594940c08a4c3594e1347957735e5ceb
Avineet Bhat <surajbhat316@gmail.com>
2023-12-21 14:21:41 +0530

    Created Routes and handler for search functionality

--- controllers/mobileController.js
+++ controllers/mobileController.js
@@ -32,6 +32,85 @@ module.exports.getMobiles = async (req, res) => {
             mobiles: []
         })
     }
-    
-    
+}
+
+module.exports.searchMobile = async (req, res)=>{
+
+    try {
+        const {searchText, filterOption } = req.body;
+        console.log(searchText);
+        console.log(filterOption);
+
+        if(filterOption ==="name"){
+            let mobiles = await Mobiles.find({name: {$regex: searchText}})
+            if(mobiles.length === 0){
+                return  res.status(200).json({
+                    message: "No mobiles found",
+                    mobiles: []
+                })
+            }
+            return res.status(200).json({
+                message: "Mobiles Found",
+                mobiles: mobiles
+            });
+        }
+        else if(filterOption ==="processor"){
+            let mobiles = await Mobiles.find({processor: {$regex: searchText}});
+            if(mobiles.length === 0){
+                return  res.status(200).json({
+                    message: "No mobiles found",
+                    mobiles: []
+                })
+            }
+            return res.status(200).json({
+                message: "Mobiles Found",
+                mobiles: mobiles
+            });
+        }
+        else if(filterOption === "type"){
+            let mobiles = await Mobiles.find({type: {$regex: searchText}});
+            if(mobiles.length === 0){
+                return  res.status(200).json({
+                    message: "No mobiles found",
+                    mobiles: []
+                })
+            }
+            return res.status(200).json({
+                message: "Mobiles Found",
+                mobiles: mobiles
+            });
+        }
+        else if(filterOption === "OS"){
+            let mobiles = await Mobiles.find({OS: {$regex: searchText}});
+            if(mobiles.length === 0){
+                return  res.status(200).json({
+                    message: "No mobiles found",
+                    mobiles: []
+                })
+            }
+            return res.status(200).json({
+                message: "Mobiles Found",
+                mobiles: mobiles
+            });
+        }
+        else{
+            let mobiles = await Mobiles.find({memory: {$regex: searchText}});
+            if(mobiles.length === 0){
+                return  res.status(200).json({
+                    message: "No mobiles found",
+                    mobiles: []
+                })
+            }
+            return res.status(200).json({
+                message: "Mobiles Found",
+                mobiles: mobiles
+            });
+        }
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+            mobiles: []
+        })
+    }
 }
--- routes/mobile.js
+++ routes/mobile.js
@@ -5,8 +5,7 @@ const router = express.Router();
 const mobileController = require("../controllers/mobileController");
 
 router.get("/", mobileController.getMobiles);
-
-
+router.post("/search", mobileController.searchMobile);
 
 
 module.exports = router;
